63ab7a8605e52a4b34e9608be389bcb2b1ccd55a
ItzDommo <Justinleitol@gmail.com>
2025-11-21 17:09:13 +0100

    Deploy ready

--- ClickNFish/src/services/http.ts
+++ ClickNFish/src/services/http.ts
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.PROD
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:8080/api";
-
 export const http = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-
